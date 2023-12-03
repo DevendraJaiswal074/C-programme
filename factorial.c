8af9a7f1a0e3a9b49d923691ec6b7f1f14a308c1
Devendrajaiswal <109809511+DevendraJaiswal074@users.noreply.github.com>
2023-12-03 16:05:51 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -5,7 +5,7 @@ int main()
     printf("ENTER A NUMBER: ");
     scanf("%d",user);
 
-    int fact=1;                                             //incomplte program
+    int fact=1;                                             
     for(int i=1 ;i<=user; i++){
         fact= fact * i;
     }
